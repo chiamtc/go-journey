91925ed730ff6bee4a9e8046f595c5fd4ea1218b
Chiam Tat Cheng <chiamcheng@gmail.com>
2019-12-10 20:41:45 +1000

    golang helloworld

---
+++ go_helloworld/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world")
+	fmt.Printf("hello word from printF")
+}
+
+/*
+go run = go run xx.go = executes the go file
+go build = go build = compile all the go files in the dir
+go fmt =  format all the code in each file
+go install = compiles and install a dependency
+go get = donwloads raw source code of someone else's package
+go test = test
+
+*/
